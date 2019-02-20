30db30d2306b7ed2df3b1d4df7b67d0eb54187c8
HugoAndres <huggo.ipn.1997@gmail.com>
2019-02-20 14:27:14 -0600

    Agregue dos tipo de dato en la estructura <Estudiante>, cree una nueva estructura llamada <evaluacion> y defini una funcion llamada <Calcular_promedio>.

--- estudiante.h
+++ estudiante.h
@@ -13,10 +13,20 @@ typedef struct  {
     char nombre[SIZE];
     char apPaterno[SIZE];
     char apMaterno[SIZE];
+    evaluacion actividadesevaluar[4]; //Son las 4 actividades que debeevaluar.
+    float promedio; //tenra el promedio de cada alumno
 } Estudiante;
+
+//Defini la estructura para poder evaluar las actividades.
+typedef struct  {
+     actividad nombredeactividad;
+     float calificacion;
+     char observaciones[TAM];
+}evaluacion;
 
 
 void Estudiante_registrar(Estudiante *alumno);
 void Estudiante_mostrar(Estudiante alumno);
+float Calcular_promedio(Estudiante * e); // Esta funcion servira para calcular el promedio total del alumno.
 
 #endif
