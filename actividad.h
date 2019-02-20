9cc46f9a7b9d3ed913ebe530e361b570c2875ad7
HugoAndres <huggo.ipn.1997@gmail.com>
2019-02-20 14:31:42 -0600

    Agregue un tipo de dato a la estrcutura <Porcentaje> pero no se como declarar ahi los porcentajes que tendra cada actividad o si eso lo hago desde el main?.

--- actividad.h
+++ actividad.h
@@ -13,6 +13,7 @@ typedef struct  {
     int identificador;
     char descripcion[SIZE];
     Fecha fechaLimite;
+    float porcentaje; //Puede que aqui pendere el porcentaje de cada actividad, pero no se como imprementarlo.
 } Actividad;
 
 
