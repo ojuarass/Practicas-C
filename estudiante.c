12ff26b7f95bb911e05c43d4d662f0ee2a4df65d
HugoAndres <huggo.ipn.1997@gmail.com>
2019-02-20 14:28:46 -0600

    Implemente la funcion <Calcular_promedio>

--- estudiante.c
+++ estudiante.c
@@ -28,5 +28,18 @@ void Estudiante_mostrar(Estudiante alumno){
     printf("============== Datos del estudiante  ================\n");
     printf("Matricula: %d \n", alumno.matricula);
     printf("Nombre: %s %s %s \n", alumno.apPaterno, alumno.apMaterno, alumno.nombre);
-    
 }
+
+//Agregue esta funcion para calcular el promedio de cada estudiante.
+float Calcular_promedio(Estudiante * e){
+        int i = 0;
+        float prom = 0;
+        for(i = 0; i < e->actividadesevaluar;i++) {
+                //Quiero sacar el promedio dependiendo la actividad, pero no se como y donde ponderar el porcentaje de cada actividad.
+        prom = (e->actividadesevaluar[i].calificacion) * (e -> evaluacion[i].actividad.porcentaje);
+        }
+        e->promedio = prom;
+        return prom;
+}
+                                                    44,1        Final
+
